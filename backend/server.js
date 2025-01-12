725a26ddcf8f1e82adfc26087846de4c5246f2b8
krish <krishrajani680@gmail.com>
2025-01-11 21:57:29 -0700

    testing write

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
 const axios = require("axios");
 
 async function getFuelPrices(){
+
     //get the data from the server that is currently running
     const response = await axios.get("http://127.0.0.1:5000/Alberta/edmonton");
-
-
+    return response.data;
 }
+
+app.get("/", async function(req, res){
+    fuel_prices = await getFuelPrices();
+    res.send(fuel_prices);
+
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+})

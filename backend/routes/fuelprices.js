276c7bc7fac4047f404803f8fd602eeb0e14e162
krish <krishrajani680@gmail.com>
2025-01-12 22:50:43 -0700

    store data in json

--- backend/routes/fuelprices.js
+++ backend/routes/fuelprices.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const axios = require("axios");
+const fs = require("fs");
 
 
 
@@ -24,15 +25,16 @@ db.once("open", async() => {
 
     const response = await axios.get("http://127.0.0.1:5000/Alberta/edmonton");
     const fuel_data = response.data;
-    await fuelPrice.insertMany(fuel_data);
-    
-    
+    //await fuelPrice.insertMany(fuel_data);
+
+    fs.writeFile("fuel_data.json", JSON.stringify(fuel_data, null, 2), (err) => {
+        if(err){
+            console.log(err);
+        }
+    });
 
     const prices = await fuelPrice.find({}, {_id: 0, price: 1});
     console.log(prices);
-
-
-
     mongoose.connection.close();
     
 });

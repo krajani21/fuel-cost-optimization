0bf621e2eaaf6ace473334b81660dd5ff1cbcf4c
krish <krishrajani680@gmail.com>
2025-06-26 15:18:28 -0600

    changed distanceRoutes.js to make a get request on server fetching fuel stations dynamically

--- backend/routes/distanceRoutes.js
+++ backend/routes/distanceRoutes.js
@@ -1,10 +1,8 @@
-// backend/routes/distanceRoutes.js
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const stations = require("../data/fuel_data.json"); //for now, get data from local JSON file
-//later will change to database
 
+const FLASK_API_URL = "http://localhost:8000/alberta/edmonton";
 
 router.post("/", async (req, res) => {
   try {
@@ -14,10 +12,13 @@ router.post("/", async (req, res) => {
       return res.status(400).json({ error: "Invalid origin location" });
     }
 
-    //starting point for calculating distance
+      //starting point for calculating distance
     const originString = `${origin.lat},${origin.lng}`;
 
-    const city = "Edmonton, AB, Canada";//default city for now can be changed later
+    const city = "Edmonton, AB, Canada";//default city for now can be changed later 
+
+    const stationRes = await axios.get(FLASK_API_URL);
+    const stations = stationRes.data;
 
     //get all destinations to calculate distances to
     const destinations = stations
@@ -27,17 +28,14 @@ router.post("/", async (req, res) => {
     const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${originString}&destinations=${destinations}&key=${process.env.GOOGLE_API_KEY}`;
 
     const response = await axios.get(url);
-    console.log("Google API raw response:", response.data);
-
-
     const distanceData = response.data.rows[0].elements;
 
-    // Attach distance data to station list
+    //attach distance data to station list
     const result = stations.map((station, index) => ({
       ...station,
-      distance: distanceData[index].distance?.value || null, // in meters
+      distance: distanceData[index].distance?.value || null,
       distance_text: distanceData[index].distance?.text || null,
-      duration: distanceData[index].duration?.value || null, // in seconds
+      duration: distanceData[index].duration?.value || null,
       duration_text: distanceData[index].duration?.text || null,
     }));
 
